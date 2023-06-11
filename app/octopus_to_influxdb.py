4d8d6bd45d69a2160ef10dd9f4ac6be085c93893
Ruben d'Arco <cyclops-github@prof-x.net>
2023-06-11 20:37:05 +0100

    Updates
    - move to influxdb v2 client.
    - Add --no-gas option as i don't have octopus gas.

--- app/octopus_to_influxdb.py
+++ app/octopus_to_influxdb.py
@@ -6,7 +6,9 @@ from urllib import parse
 import click
 import maya
 import requests
-from influxdb import InfluxDBClient
+
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 def retrieve_paginated_data(
@@ -131,7 +133,31 @@ def store_series(connection, series, metrics, rate_data):
         }
         for measurement in metrics
     ]
-    connection.write_points(measurements)
+    write_api = connection.write_api(write_options=SYNCHRONOUS)
+
+    #print(measurements)
+    for measurement in measurements:
+        print(f"Measurement for {measurement['time']}:\n")
+        for field in measurement['fields']:
+            print(f"\tField: {field}:{measurement['fields'][field]}")
+            point = Point("electricity").field(field, measurement['fields'][field]).time(measurement['time'])
+            write_api.write(bucket="energy", record=point)
+
+#    points = []
+#    for measurement in metrics:
+
+    #print(f"Measurement: {measurements}")
+        # point = Point("weather")\
+        #     .tag("location", "New York")\
+        #     .field("temperature", random.randint(-10, 30))\
+        #     .time(now - timedelta(days=i))
+        # points.append(point)
+
+
+#{'consumption': 0.091, 'interval_start': '2023-06-09T00:30:00+01:00', 'interval_end': '2023-06-09T01:00:00+01:00'}
+    
+ #   write_api = connection.write_api(write_options=SYNCHRONOUS)
+  #  connection.write_points(measurements)
 
 
 @click.command()
@@ -142,18 +168,13 @@ def store_series(connection, series, metrics, rate_data):
 )
 @click.option('--from-date', default='yesterday midnight', type=click.STRING)
 @click.option('--to-date', default='today midnight', type=click.STRING)
-def cmd(config_file, from_date, to_date):
+@click.option('--no-gas', default=False, type=click.BOOL)
+def cmd(config_file, from_date, to_date, no_gas):
 
     config = ConfigParser()
     config.read(config_file)
 
-    influx = InfluxDBClient(
-        host=config.get('influxdb', 'host', fallback="localhost"),
-        port=config.getint('influxdb', 'port', fallback=8086),
-        username=config.get('influxdb', 'user', fallback=""),
-        password=config.get('influxdb', 'password', fallback=""),
-        database=config.get('influxdb', 'database', fallback="energy"),
-    )
+    influx = InfluxDBClient.from_config_file(config_file)
 
     api_key = config.get('octopus', 'api_key')
     if not api_key:
@@ -163,8 +184,8 @@ def cmd(config_file, from_date, to_date):
     e_serial = config.get('electricity', 'serial_number', fallback=None)
     if not e_mpan or not e_serial:
         raise click.ClickException('No electricity meter identifiers')
-    e_url = 'https://api.octopus.energy/v1/electricity-meter-points/' \
-            f'{e_mpan}/meters/{e_serial}/consumption/'
+    e_url = f"https://api.octopus.energy/v1/electricity-meter-points/{e_mpan}/meters/{e_serial}/consumption/"
+    click.echo(e_url)
     agile_url = config.get('electricity', 'agile_rate_url', fallback=None)
 
     g_mpan = config.get('gas', 'mpan', fallback=None)
@@ -173,7 +194,8 @@ def cmd(config_file, from_date, to_date):
     g_vcf = config.get('gas', 'volume_correction_factor', fallback=1.02264)
     g_cv = config.get('gas', 'calorific_value', fallback=40)
     if not g_mpan or not g_serial:
-        raise click.ClickException('No gas meter identifiers')
+        if no_gas == False:
+            raise click.ClickException('No gas meter identifiers')
     g_url = 'https://api.octopus.energy/v1/gas-meter-points/' \
             f'{g_mpan}/meters/{g_serial}/consumption/'
 
@@ -219,29 +241,25 @@ def cmd(config_file, from_date, to_date):
         f'Retrieving electricity data for {from_iso} until {to_iso}...',
         nl=False
     )
-    e_consumption = retrieve_paginated_data(
-        api_key, e_url, from_iso, to_iso
-    )
+    e_consumption = retrieve_paginated_data(api_key, e_url, from_iso, to_iso )
     click.echo(f' {len(e_consumption)} readings.')
     click.echo(
         f'Retrieving Agile rates for {from_iso} until {to_iso}...',
         nl=False
     )
-    rate_data['electricity']['agile_unit_rates'] = retrieve_paginated_data(
-        api_key, agile_url, from_iso, to_iso
-    )
+    rate_data['electricity']['agile_unit_rates'] = retrieve_paginated_data( api_key, agile_url, from_iso, to_iso)
     click.echo(f' {len(rate_data["electricity"]["agile_unit_rates"])} rates.')
+    click.echo(rate_data["electricity"]["agile_unit_rates"])
     store_series(influx, 'electricity', e_consumption, rate_data['electricity'])
 
-    click.echo(
-        f'Retrieving gas data for {from_iso} until {to_iso}...',
-        nl=False
-    )
-    g_consumption = retrieve_paginated_data(
-        api_key, g_url, from_iso, to_iso
-    )
-    click.echo(f' {len(g_consumption)} readings.')
-    store_series(influx, 'gas', g_consumption, rate_data['gas'])
+    if no_gas == False:
+        click.echo(
+            f'Retrieving gas data for {from_iso} until {to_iso}...',
+            nl=False
+        )
+        g_consumption = retrieve_paginated_data(api_key, g_url, from_iso, to_iso)
+        click.echo(f' {len(g_consumption)} readings.')
+        store_series(influx, 'gas', g_consumption, rate_data['gas'])
 
 
 if __name__ == '__main__':
